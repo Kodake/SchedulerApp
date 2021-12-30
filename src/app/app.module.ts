7831c00ec5301637d9fb0e19a6035adaa41e24dd
R-Kazukio <rflorentino@wepsys.net>
2021-12-30 02:31:59 -0400

    All changes about SchedulerApp using syncfusion angular schedule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, YearService, MonthAgendaService } from '@syncfusion/ej2-angular-schedule';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScheduleModule
   ],
-  providers: [],
+  providers: [RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, YearService, MonthAgendaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
